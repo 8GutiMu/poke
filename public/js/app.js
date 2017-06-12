d96fc1100a3af53b92aee683730561cb9fc99bdb
8GutiMu <you@example.com>
2017-06-12 15:47:18 -0500

    ctrl save

--- public/js/app.js
+++ public/js/app.js
@@ -5,12 +5,13 @@ var plantillaPokemon = '<ul>' +
 
 var cargarPagina = function () {
     cargarPokemones();
-    $(document).on("click", ".pokemon", entrarAPokemon);
-    console.log("hola")
+    $(document).on("click", ".pokemon", mostrarPokemon);
+    $(".pokemon").modal();
+
 };
 
 var cargarPokemones = function () {
-    var url = 'https://pokeapi.co/api/v2/pokemon/';
+    var url = 'https://pokeapi.co/api/v2/pokemon-species/';
     $.getJSON(url, function (response) {
         var pokemons = response.results;
         mostrarPokemones(pokemons);
@@ -28,11 +29,15 @@ var mostrarPokemones = function (pokemons) {
     })
 }
 
-var entrarAPokemon = function () {
+var mostrarPokemon = function () {
     var url = ($(this).data("url"));
     console.log(url)
     $.getJSON(url, function (response) {
-        console.log("dentro", response.abilities[0])
+        $("#nombre_modal").html("<h6><strong>Color: </strong>" + response.color.name + "</h6>");
+        $("#habitad_modal").html("<h6><strong>Habitad: </strong>" + response.habitat.name + "</h6>");
+        $("#shape_modal").html("<h6><strong>Forma: </strong>" + response.shape.name + "</h6>")
+        $("#genera_modal").html("<h6><strong>Genera: </strong>" + response.genera[0].genus + "</h6>");
+
     })
 
 }
